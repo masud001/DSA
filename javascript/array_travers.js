73ebbfd26f26c1a852fb2d09a6703ae6ee896441
masud001 <madhnagar@gmail.com>
2024-08-29 01:34:40 +0600

    (dsa) array

---
+++ javascript/array_travers.js
@@ -0,0 +1,9 @@
+// Date: 2021/05/30
+console.log("Hello, array travers");
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < arr.length; i++) {
+  console.log("index: ", i, "value: ", arr[i]);
+}
+//access 4th element of this array
+console.log("4th element: ", arr[3]);
