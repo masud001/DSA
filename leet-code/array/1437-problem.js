87ed817b38135e5d8a9881516b71b66c9ac2cf1b
masud001 <madhnagar@gmail.com>
2025-03-04 00:48:49 +0600

    good array

--- leet-code/array/1437-problem.js
+++ leet-code/array/1437-problem.js
@@ -1,16 +1,3 @@
-//?  Given an binary array nums and an integer k, return true if all 1's are at least k places away from each other, otherwise return false.
-//?  Example 1:
-//?  Input: nums = [1,0,0,1,0,1], k = 2
-//?  Output: true
-//?  Explanation: Each of the 1s are at least 2 places away from each other.
-//?  Example 2:
-//?  Input: nums = [1,0,0,1,1,0,0,1], k = 2
-//?  Output: false
-//?  Explanation: The second 1 and third 1 are only one apart from each other.
-//?  Example 3:
-//?  Input: nums = [1,1,1,1,1], k = 0
-//?  Output: true
-//?  Explanation: There are 5 1s, which is more than 0 places apart.
 var kLengthApart = function (nums, k) {
   let count = 0;
   for (let i = 0; i < nums.length; i++) {
